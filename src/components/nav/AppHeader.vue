27f65c8d8ba4e59e85c284a8da68fca7820f0915
Jeff Hartman <dracul104@gmail.com>
2023-12-05 22:44:37 -0500

    static size app header

--- src/components/nav/AppHeader.vue
+++ src/components/nav/AppHeader.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="bg-blue-600 text-white p-4 shadow-md flex justify-between items-center">
-    <div v-if="route.path.includes('/bill/')">
-      <span @click="goBack" class="cursor-pointer text-2xl mr-2">&larr;</span>
-      <span class="text-xl font-bold">Details</span>
+  <div class="h-16 bg-blue-600 text-white p-4 shadow-md flex justify-between items-center">
+    <div class="flex items-center">
+      <!-- Left content (back arrow or hamburger) -->
+      <span v-if="route.path.includes('/bill/')" @click="goBack" class="cursor-pointer text-2xl mr-2">&larr;</span>
+      <span class="text-xl font-bold" v-if="route.path.includes('/bill/')">Details</span>
+      <span class="text-xl font-bold" v-else>Bills</span>
     </div>
-    <div v-else>
-      <!-- Hamburger menu icon -->
-      <span @click="toggleMenu" class="cursor-pointer text-2xl mr-2">
-        &#9776;
-      </span>
-      <span class="text-xl font-bold">Bills</span>      
+
+    <div v-if="route.path.includes('/bill/')" class="flex items-center">
+      <!-- Right content (edit and delete buttons) -->
+      <button @click="editBill" class="p-2">
+        <!-- Edit icon (SVG) --> Edit
+      </button>
+      <button @click="deleteBill" class="p-2">
+        <!-- Delete icon (SVG) --> Delete
+      </button>
     </div>
   </div>
   
