2535bf76ac68796ac5a28a5a07c0f42f2554bec1
Jeff Hartman <dracul104@gmail.com>
2023-12-05 22:54:23 -0500

    add svg icons

--- src/components/nav/AppHeader.vue
+++ src/components/nav/AppHeader.vue
@@ -2,7 +2,18 @@
   <div class="h-16 bg-blue-600 text-white p-4 shadow-md flex justify-between items-center">
     <div class="flex items-center">
       <!-- Left content (back arrow or hamburger) -->
-      <span v-if="route.path.includes('/bill/')" @click="goBack" class="cursor-pointer text-2xl mr-2">&larr;</span>
+      <span v-if="route.path.includes('/bill/')" @click="goBack" class="cursor-pointer text-2xl mr-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+          <path fill-rule="evenodd" d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clip-rule="evenodd" />
+        </svg>
+      </span>
+      <span v-else @click="toggleMenu" class="cursor-pointer text-2xl mr-2">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+          <path fill-rule="evenodd" d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+        </svg>
+      </span>
+
+      <!-- Page Title -->
       <span class="text-xl font-bold" v-if="route.path.includes('/bill/')">Details</span>
       <span class="text-xl font-bold" v-else>Bills</span>
     </div>
@@ -10,10 +21,14 @@
     <div v-if="route.path.includes('/bill/')" class="flex items-center">
       <!-- Right content (edit and delete buttons) -->
       <button @click="editBill" class="p-2">
-        <!-- Edit icon (SVG) --> Edit
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+          <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-12.15 12.15a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32L19.513 8.2z" />
+        </svg>
       </button>
       <button @click="deleteBill" class="p-2">
-        <!-- Delete icon (SVG) --> Delete
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+          <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clip-rule="evenodd" />
+        </svg>
       </button>
     </div>
   </div>
